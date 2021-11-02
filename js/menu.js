6b31e79ec1b1a785e67bbf6075a17fd40be51270
Daniel Scally <44758401+dascally@users.noreply.github.com>
2021-11-02 00:05:43 -0400

    Styling: Add shadow to menu item info popovers

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,7 @@ import { Popover } from 'bootstrap/dist/js/bootstrap.esm';
 const popovers = document.querySelectorAll('.menu-item-info[data-bs-toggle="popover"]');
 for (const elt of popovers) {
   new Popover(elt, {
-    template: `<div class="popover" role="tooltip">\
+    template: `<div class="popover shadow-sm" role="tooltip">\
     <div class="popover-arrow"></div>\
     <h4 class="popover-header fs-6"></h4>\
     <div class="popover-body"></div>\
