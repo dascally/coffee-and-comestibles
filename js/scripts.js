74f8bd0d76fb973d20332ef2794c8355270eb488
Daniel Scally <44758401+dascally@users.noreply.github.com>
2021-11-01 22:45:58 -0400

    Menu: Fix bug with initializing popovers
    Script that initializes popovers is now a module and imports Popover
    from bootstrap.esm.js rather than trying to use bootstrap in the
    global scope.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
+import { Popover } from '../node_modules/bootstrap/dist/js/bootstrap.esm.js';
+
 const popovers = document.querySelectorAll('.menu-item-info[data-bs-toggle="popover"]');
 for (const elt of popovers) {
-  new bootstrap.Popover(elt, {
+  new Popover(elt, {
     template: `<div class="popover" role="tooltip">\
     <div class="popover-arrow"></div>\
     <h4 class="popover-header fs-6"></h4>\
